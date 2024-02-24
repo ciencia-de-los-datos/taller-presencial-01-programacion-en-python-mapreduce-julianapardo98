bd0ff6a23eefb7d7fbd67eb4a409ac20e90c66b4
julianapardo98 <160270630+julianapardo98@users.noreply.github.com>
2024-02-23 20:36:19 -0500

    Primer intento

--- word_count.py
+++ word_count.py
@@ -13,8 +13,16 @@
 #     ('text2.txt'. 'hypotheses.')
 #   ]
 #
+import glob
+import fileinput
+
 def load_input(input_directory):
-    pass
+    sequence = []
+    filenames = glob.glob(input_directory + "/*") #glob permite leer contenido de una carpeta, /* para que me aparezca todo lo que esta en el archivo
+    with fileinput.input(files=filenames) as f:         #el fileinput lo vuelve iterador
+        for line in f:          #para devolver una a una las lineas de los archivos uno a uno
+            sequence.append((fileinput.filename(), line))
+    return sequence  #genere lista de tuplas con las lineas de los archivos
 
 
 #
@@ -29,8 +37,17 @@ def load_input(input_directory):
 #     ...
 #   ]
 #
-def mapper(sequence):
-    pass
+def mapper(sequence):               #defino la función mapper que depende de sequence
+    new_sequence = []               
+    for _, text in sequence:         #asigno nombre de archivo y texto, _ coge el ultimo reusultado
+        words = text.split()            #separa las palabras
+        for word in words:
+            word = word.replace(",", "")
+            word = word.replace(".", "")
+            word = word.lower()
+            new_sequence.append((word, 1))  #agrega los elementos al final de la lista
+    return new_sequence
+#esto nos hace el MAp, duplas en lista con valor
 
 
 #
@@ -43,10 +60,10 @@ def mapper(sequence):
 #     ('Analytics', 1),
 #     ...
 #   ]
-#
+#La unica tarea de esta funcion es ordenar la secuencia
 def shuffle_and_sort(sequence):
-    pass
-
+    sorted_sequence = sorted(sequence, key=lambda x: x[0]) #lambda es funcion anonima que recibe x y devuelve x[0]
+    return sorted_sequence
 
 #
 # Escriba la función reducer, la cual recibe el resultado de shuffle_and_sort y
@@ -55,15 +72,29 @@ def shuffle_and_sort(sequence):
 # texto.
 #
 def reducer(sequence):
-    pass
+    diccionario = {}
+    for key, value in sequence:
+        if key not in diccionario.keys():  #si no le agrego, no me permite asignar valores porque no existe ninguna key
+            diccionario[key] = []   #a la key le agrego una lista vacía como valor
+        diccionario[key].append(value)   #luego a esa key, al valor (que antes era vacio) agrego value
+
+    new_sequence = []
+    for key, value in diccionario.items():
+        tupla = (key, sum(value))
+        new_sequence.append(tupla)
+    return new_sequence
 
 
 #
 # Escriba la función create_ouptput_directory que recibe un nombre de directorio
 # y lo crea. Si el directorio existe, la función falla.
 #
-def create_ouptput_directory(output_directory):
-    pass
+import os.path   #permite hacer operaciones en archivos
+
+def create_output_directory(output_directory):
+    if os.path.exists(output_directory):   #para ver si el archivo existe
+        raise FileExistsError(f"The directory '{output_directory}' already exists")
+    os.makedirs(output_directory)
 
 
 #
@@ -75,7 +106,9 @@ def create_ouptput_directory(output_directory):
 # separados por un tabulador.
 #
 def save_output(output_directory, sequence):
-    pass
+    with open(output_directory + "/part-00000", "w") as file:
+        for key, value in sequence:
+            file.write(f"{key}\t{value}\n")
 
 
 #
@@ -83,17 +116,23 @@ def save_output(output_directory, sequence):
 # entregado como parámetro.
 #
 def create_marker(output_directory):
-    pass
-
+    with open(output_directory + "/_SUCCES", "w") as file:   #este archivo vacio es para decir que quedo bien
+        file.write("")
 
 #
 # Escriba la función job, la cual orquesta las funciones anteriores.
 #
 def job(input_directory, output_directory):
-    pass
+    sequence = load_input(input_directory)
+    sequence = mapper(sequence)
+    sequence = shuffle_and_sort(sequence)
+    sequence = reducer(sequence)
+    create_output_directory(output_directory)
+    save_output(output_directory, sequence)
+    create_marker(output_directory)
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":   #para que solo se llame job cuando llamo el archivo word_count
     job(
         "input",
         "output",
